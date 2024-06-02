f80995042c9f096d04371e4d58d1974cb3667249
Duc Nguyen <minhduc140583@gmail.com>
2024-06-02 17:59:27 +0700

    Apply new version

--- main.go
+++ main.go
@@ -12,26 +12,25 @@ import (
 )
 
 func main() {
-	var conf app.Config
-	err := config.Load(&conf, "configs/config")
+	var cfg app.Config
+	err := config.Load(&cfg, "configs/config")
 	if err != nil {
 		panic(err)
 	}
-	log.Initialize(conf.Log, rotatelogs.GetWriter)
-	fmt.Println("Import file")
+	log.Initialize(cfg.Log, rotatelogs.GetWriter)
 	ctx := context.Background()
 	log.Info(ctx, "Import file")
-	app, err := app.NewApp(ctx, conf)
+	app, err := app.NewApp(ctx, cfg)
 	if err != nil {
 		log.Errorf(ctx, "Error when initialize: %v", err)
 		panic(err)
 	}
 
 	total, success, err := app.Import(ctx)
-	fmt.Println(fmt.Sprintf("total: %d, success: %d", total, success))
 	if err != nil {
 		log.Errorf(ctx, "Error when import: %v", err)
 		panic(err)
 	}
+	log.Info(ctx, fmt.Sprintf("total: %d, success: %d", total, success))
 	log.Info(ctx, "Imported file")
 }
